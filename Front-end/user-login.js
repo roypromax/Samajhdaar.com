9b1f21076f2996f69d5174dede000bfedd504b42
Rishabh Roy <roy19rishabh@gmail.com>
2023-01-22 14:39:43 +0530

    Updated login page and admin login redirected

--- Front-end/user-login.js
+++ Front-end/user-login.js
@@ -1,1 +1,12 @@
 // login functionality
+
+let myForm = document.getElementById("login");
+
+myForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    
+    if(myForm.username.value == "admin" && myForm.password.value == "admin"){
+        window.location = "../Backend/fashionfuion/fashionfuion/Admin.html";
+    }
+})
+
