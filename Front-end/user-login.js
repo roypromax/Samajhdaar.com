ae6c494d5025d4c1691b4659ba2485c7319a7a15
Rishabh Roy <roy19rishabh@gmail.com>
2023-01-22 16:00:16 +0530

    user-login page updated

--- Front-end/user-login.js
+++ Front-end/user-login.js
@@ -4,8 +4,10 @@ let myForm = document.getElementById("login");
 
 myForm.addEventListener("submit",(e)=>{
     e.preventDefault();
-    
-    if(myForm.username.value == "admin" && myForm.password.value == "admin"){
+
+    if(myForm.username.value == "" || myForm.password.value == ""){
+        alert("Please enter details")
+    }else if(myForm.username.value == "admin" && myForm.password.value == "admin"){
         window.location = "../Backend/fashionfuion/fashionfuion/Admin.html";
     }
 })
